897c6eaf62ea20790c007281daac821fe8ec9301
Joshua-Pok <official.joshuapok@gmail.com>
2026-02-25 20:21:07 +0800

    basic starter template

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string_view>
+
+int main(int argc, char** argv){
+	(void)argc;
+	(void)argv;
+
+
+	std::cout << "csv_analytics\n"
+		  << "Usage: csv_analytics [--help]\n"
+		  << "Step1 placeholder: build + test scaffolding";
+
+
+	return 0;
+}
